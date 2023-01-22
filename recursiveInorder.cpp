a6367f4a91d6834a30a959885d811debab93766f
harkeshbirman <harkeshbirman777@outlook.com>
2023-01-22 23:37:00 +0530

    recurseiveInorder.cpp added

---
+++ recursiveInorder.cpp
@@ -0,0 +1,14 @@
+// struct treenode {
+//     int val;
+//     treenode *left,
+//     treenode *right
+// }
+
+void inOrder(treenode *root) {
+    if(root == NULL) {
+		return;
+    }
+    inOrder(root->left);
+    cout << root->val << " ";
+    inOrder(root->right);
+}
